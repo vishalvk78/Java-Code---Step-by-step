630e5835e7e3966f1983cba0b279c5c107f71b6f
Vishal Kadam <85665373+vishalvk78@users.noreply.github.com>
2022-10-07 11:52:52 +0530

    Update Array_1.java

--- Array/Array_1.java
+++ Array/Array_1.java
@@ -1,14 +1,16 @@
 public class Main
 {
 	public static void main(String[] args) {
-		int[] arr; //decaration
+		int[] arr; //declaration
 		arr=new int[5]; //memory allocation
+		//int[] arr={1,2,3,4,5}; //declaration +  initilization
 		arr[0]=1; //initilization
 		arr[1]=2;
 		arr[2]=3;
 		arr[3]=4;
 		arr[4]=5;
 		
+		//Print output
 		for(int i=0;i<5;i++)
 		{
 		    System.out.print(arr[i] + " ");
