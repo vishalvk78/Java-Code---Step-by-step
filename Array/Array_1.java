6f55c8d0e6fb021a8036109e56d5468fd36634f3
Vishal Kadam <85665373+vishalvk78@users.noreply.github.com>
2022-10-07 11:47:03 +0530

    Create Array_1.java

---
+++ Array/Array_1.java
@@ -0,0 +1,18 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int[] arr;
+		arr=new int[5];
+		arr[0]=1;
+		arr[1]=2;
+		arr[2]=3;
+		arr[3]=4;
+		arr[4]=5;
+		
+		for(int i=0;i<5;i++)
+		{
+		    System.out.print(arr[i] + " ");
+		}
+		
+	}
+}
