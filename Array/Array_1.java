d3a0958ae15c7df5ba491a4a7b4bf0e3b68cb3ea
Vishal Kadam <85665373+vishalvk78@users.noreply.github.com>
2022-10-07 12:29:36 +0530

    Update Array_1.java

--- Array/Array_1.java
+++ Array/Array_1.java
@@ -16,5 +16,10 @@ public class Main
 		    System.out.print(arr[i] + " ");
 		}
 		
+		String [] name={"Vishal", "Sham", "Kadam", "Ram"};
+		
+		System.out.println(name[0]);
+		System.out.println(name.length);
+		
 	}
 }
