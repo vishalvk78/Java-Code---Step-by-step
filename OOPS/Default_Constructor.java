1562112544fb2092b480e108843da9e8b98990c9
Vishal Kadam <85665373+vishalvk78@users.noreply.github.com>
2022-10-10 23:35:03 +0530

    Create Default_Constructor.java

---
+++ OOPS/Default_Constructor.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+Default Constructor
+*******************************************************************************/
+class Employee
+{
+    Employee()
+    {
+        System.out.println("Vishal Kadam");
+    }
+    
+    Employee(int n){
+        
+    }
+}
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+		Employee emp1 = new Employee();
+		
+	}
+}
